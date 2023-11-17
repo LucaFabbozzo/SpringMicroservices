90b07169a24c9bbd0060e5adc7bf894088749d1d
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-11-17 17:32:25 +0100

    changes

---
+++ SpringMicroservices/microservice-course/src/main/java/com/microservice/course/entities/Course.java
@@ -0,0 +1,19 @@
+package com.microservice.course.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String teacher;
+}
