0eadc619d5dbec80279dc7b60e94c530fc91d060
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-11-17 12:18:39 +0100

    configuration eureka server

--- SpringMicroservices/microservice-eureka/src/test/java/com/microservice/eureka/MicroserviceEurekaApplicationTests.java
+++ SpringMicroservices/microservice-eureka/src/test/java/com/microservice/eureka/MicroserviceEurekaApplicationTests.java
@@ -2,6 +2,8 @@ package com.microservice.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootTest
 class MicroserviceEurekaApplicationTests {
