d36d367dfd237a002898b30a823d03471112e253
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-11-19 11:51:23 +0100

    msvs gateway configuration

--- SpringMicroservices/microservice-course/src/main/java/com/microservice/course/controller/CourseController.java
+++ SpringMicroservices/microservice-course/src/main/java/com/microservice/course/controller/CourseController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/courses")
+@RequestMapping("/api/course")
 public class CourseController {
     @Autowired
     private ICourseService courseService;
